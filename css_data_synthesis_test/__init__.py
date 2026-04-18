88519907a48e6de1858b95668a04043285e13b95
GJ <gj@GJdeMacBook-Air.local>
2026-04-18 20:27:36 +0800

    uat

--- css_data_synthesis_test/__init__.py
+++ css_data_synthesis_test/__init__.py
@@ -1,4 +1,4 @@
-from multi_agent_data_synthesis.config import AppConfig, load_config
-from multi_agent_data_synthesis.orchestrator import DialogueOrchestrator
+from css_data_synthesis_test.config import AppConfig, load_config
+from css_data_synthesis_test.orchestrator import DialogueOrchestrator
 
 __all__ = ["AppConfig", "DialogueOrchestrator", "load_config"]
